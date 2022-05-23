3b50d86b548ef851569d6746902ba194257e1f94
Matthew Wang <wmatthew123@gmail.com>
2022-05-23 13:32:53 +0800

    reach

---
+++ pages/work/reach.tsx
@@ -0,0 +1,48 @@
+import ArticleLayout from '../../components/ArticleLayout';
+import Image from 'next/image';
+
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import { GetStaticProps } from 'next';
+
+const name = 'reach';
+const translationPath = 'workReach';
+
+import imageOne from '../../public/images/works/reach/1.jpg'
+import imageTwo from '../../public/images/works/reach/2.jpg';
+import imageThree from '../../public/images/works/reach/3.jpg';
+
+
+const Reach = () => {
+
+    const { t } = useTranslation(translationPath);
+
+    return (
+        <ArticleLayout>
+            <h1>{t('name')}</h1>
+            <p>{t('medium')}</p>
+            <p>{t('size')}</p>
+            <div className="grid grid-cols-3 gap-1 md:gap-4">
+                <Image src={imageOne} alt={name} placeholder={'blur'} sizes='30vw' />
+                <Image src={imageTwo} alt={name} placeholder={'blur'} sizes='30vw' />
+                <Image src={imageThree} alt={name} placeholder={'blur'} sizes='30vw' />
+            </div>
+            <p>{t('description1')}</p>
+            <Image src={imageOne} alt={name} placeholder={'blur'} />
+            <Image src={imageTwo} alt={name} placeholder={'blur'} />
+            <Image src={imageThree} alt={name} placeholder={'blur'} />
+        </ArticleLayout>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale as string, [translationPath, 'common'])),
+            // Will be passed to the page component as props
+        },
+    };
+}
+
+export default Reach;
---
+++ pages/work/surpass.tsx
@@ -0,0 +1,48 @@
+import ArticleLayout from '../../components/ArticleLayout';
+import Image from 'next/image';
+
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import { GetStaticProps } from 'next';
+
+const name = 'surpass';
+const translationPath = 'workSurpass';
+
+import imageOne from '../../public/images/works/surpass/1.jpg'
+import imageTwo from '../../public/images/works/surpass/2.jpg';
+import imageThree from '../../public/images/works/surpass/3.jpg';
+
+
+const Surpass = () => {
+
+    const { t } = useTranslation(translationPath);
+
+    return (
+        <ArticleLayout>
+            <h1>{t('name')}</h1>
+            <p>{t('medium')}</p>
+            <p>{t('size')}</p>
+            <div className="grid grid-cols-3 gap-1 md:gap-4">
+                <Image src={imageOne} alt={name} />
+                <Image src={imageTwo} alt={name} />
+                <Image src={imageThree} alt={name} />
+            </div>
+            <p>{t('description1')}</p>
+            <Image src={imageOne} alt={name} className='w-full' />
+            <Image src={imageTwo} alt={name} />
+            <Image src={imageThree} alt={name} />
+        </ArticleLayout>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale as string, [translationPath, 'common'])),
+            // Will be passed to the page component as props
+        },
+    };
+}
+
+export default Surpass;
