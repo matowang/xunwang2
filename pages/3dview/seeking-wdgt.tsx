f9f73bdcc21fa6a515aa0d0439cef1344781ff6d
Matthew Wang <wmatthew123@gmail.com>
2022-05-26 16:00:42 +0800

    fix seeking wdgt

--- pages/3dview/seeking-wdgt.tsx
+++ pages/3dview/seeking-wdgt.tsx
@@ -3,23 +3,20 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 
 const Seeking3DWDGT = () => (
-    <main className="three-d-view" id="3d-seeking" >
-        <div className="keyshot-container">
-            <iframe className="three-d-view__iframe" src="/animations/seeking-xr_1217_2021.163.wdgt/seeking-xr_1217_2021.163.html" allowFullScreen frameBorder="0">
-
-            </iframe>
-        </div>
-    </main>
+    <main className="three-d-view" id="3d-seeking" dangerouslySetInnerHTML={{
+        __html:
+            `<div style="max-width: 100%; height: 100vh;">
+                    <div style="left: 0px; width: 100%; height: 100vh; position: relative; padding-bottom: 62.625%; overflow: hidden;">
+                        <iframe src="/animations/seeking_XR.4.wdgt/seeking_XR.4.html"
+                                    allowfullscreen
+                                    style="position: absolute; top: 0px; left: 0px; height: 100vh; width: 1px; min-width: 100%; *width: 100%;"
+                                    frameborder="0"
+                                    scrolling="no">
+                        </iframe>
+                    </div>
+                </div>`
+    }}></main>
 )
-
-// `
-//             <iframe src="/animations/seeking-xr_1217_2021.163.wdgt/seeking-xr_1217_2021.163.html"
-//                         allowfullscreen
-//                         style="position: absolute; top: 0px; left: 0px; height: 100vh; width: 1px; min-width: 100%; *width: 100%;"
-//                         frameborder="0"
-//                         scrolling="no">
-//             </iframe>
-//             `
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
     return {
