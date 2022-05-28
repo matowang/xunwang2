a82431a3c4806b95582ba3ae3f802fab45d81a10
Matthew Wang <wmatthew123@gmail.com>
2022-05-28 15:45:01 +0800

    new slideshow component

---
+++ components/SlideshowModal.tsx
@@ -0,0 +1,65 @@
+import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+
+import { useSpring, animated, useTransition } from 'react-spring';
+
+import IconButton from '@mui/material/IconButton';
+import Image, { StaticImageData } from 'next/image';
+
+interface SlideshowModalProps {
+    imageSrcs: StaticImageData[] | string[];
+    currentSlide: number | null;
+    setCurrentSlide: (currentSlide: number) => void;
+    closeSlides: () => void;
+    nextPage: () => void;
+    prevPage: () => void;
+}
+
+const SlideshowModal = ({ imageSrcs, currentSlide, nextPage, prevPage, closeSlides }: SlideshowModalProps) => {
+    const styles = useSpring({ display: currentSlide === null ? 'none' : 'block' });
+    const pageTransition = useTransition(currentSlide, {
+        from: {
+            opacity: 0,
+            y: '-100px'
+        },
+        enter: {
+            opacity: 1,
+            y: '0px'
+        },
+        leave: {
+            opacity: 0,
+            y: '50px'
+        },
+        trail: 130,
+        config: {
+            duration: 130
+        }
+    })
+
+    return (
+        <animated.div style={styles} className='fixed w-full h-screen z-50 top-0 left-0 bg-black'>
+            {pageTransition((styles, slide) => (
+                slide !== null && <animated.div style={styles} className="w-full h-full absolute top-0">
+                    <Image src={imageSrcs[slide]} layout="fill" objectFit="contain" />
+                </animated.div>)
+            )}
+            <div className='hidden'>
+                {imageSrcs.map((imgSrc, i) => (
+                    <Image src={imgSrc} key={imgSrc.toString() + i} />
+                ))}
+            </div>
+            <IconButton onClick={closeSlides} className='absolute top-6 right-6 !bg-stone-400'>
+                <CloseIcon />
+            </IconButton>
+            <IconButton onClick={prevPage} aria-label="back" className='absolute left-6 top-[50%] !bg-stone-400'>
+                <ArrowBackIosNewOutlinedIcon />
+            </IconButton>
+            <IconButton onClick={nextPage} aria-label="next" className='absolute right-6 top-[50%] !bg-stone-400'>
+                <ArrowForwardIosOutlinedIcon />
+            </IconButton>
+        </animated.div>
+    )
+}
+
+export default SlideshowModal;
---
+++ hooks/useSlideshowState.ts
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/router'
+
+interface UseSlideshowStateProps {
+    numOfPages: number;
+}
+
+const useSlideshowState = ({ numOfPages }: UseSlideshowStateProps) => {
+    const [currentPage, setCurrentPage] = useState<number | null>(null);
+
+    const router = useRouter();
+
+    const nextPage = () => {
+        if (currentPage === null) return;
+        if (currentPage >= numOfPages - 1)
+            return router.replace({
+                pathname: router.basePath,
+                query: { ...router.query, slide: 0 }
+            });
+        router.replace({
+            pathname: router.basePath,
+            query: { ...router.query, slide: currentPage + 1 }
+        });
+    }
+
+    const prevPage = () => {
+        if (currentPage === null) return;
+        if (currentPage === 0)
+            return router.replace({
+                pathname: router.basePath,
+                query: { ...router.query, slide: numOfPages - 1 }
+            });
+        router.replace({
+            pathname: router.basePath,
+            query: { ...router.query, slide: currentPage - 1 }
+        });
+    }
+
+    const closeSlides = () => {
+        router.push(router.basePath);
+    }
+
+    const setPage = (page: number) => {
+        router.push({
+            pathname: router.basePath,
+            query: { ...router.query, slide: page }
+        });
+    }
+
+    useEffect(() => {
+        const handleRouteChange = (url: string) => {
+            const slideParamValue = new URLSearchParams(url.split('?')[1]).get('slide');
+            const page = slideParamValue ? parseInt(slideParamValue) : null;
+            if (page && (page >= numOfPages || page < 0))
+                return setCurrentPage(null);
+            setCurrentPage(page);
+        }
+        router.events.on('routeChangeStart', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeStart', handleRouteChange);
+        }
+    }, []);
+
+
+    return {
+        currentPage,
+        setPage,
+        closeSlides,
+        nextPage,
+        prevPage
+    }
+}
+
+export default useSlideshowState;
