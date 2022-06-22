64466def4149ff186de8072eb8cd05026ac31c10
Matthew Wang <wmatthew123@gmail.com>
2022-06-22 19:18:22 +0800

    font MUI theme

---
+++ MUITheme.ts
@@ -0,0 +1,56 @@
+import { createTheme } from '@mui/material/styles';
+
+const MUITheme = createTheme({
+    typography: {
+        fontFamily: [
+            'Source Code Pro',
+            'monospace',
+        ].join(',')
+    },
+    palette: {
+        primary: {
+            main: '#a8e1ff',
+        },
+        secondary: {
+            main: 'rgba(234,114,23,0.85)',
+        },
+        background: {
+            default: '#000000',
+            paper: '#161616',
+        },
+        text: {
+            primary: 'rgba(255,255,255,0.87)',
+        },
+    },
+    components: {
+        MuiLinearProgress: {
+            styleOverrides: {
+                root: {
+                    background: 'none'
+                }
+            }
+        },
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    borderColor: '#rgba(255,255,255,.75)',
+                    '& .MuiOutlinedInput-root': {
+                        '& fieldset': {
+                            borderColor: 'rgba(255,255,255,.75)',
+                            borderRadius: 0,
+                        },
+                    },
+                    '& .MuiInputLabel-outlined': {
+                        color: 'rgba(255,255,255,.75)'
+                    },
+                    '& .Mui-disabled.MuiOutlinedInput-root': {
+                        background: '#ffffff57',
+                        color: 'rgba(255,255,255,.75)'
+                    }
+                },
+            },
+        }
+    }
+});
+
+export default MUITheme;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,11 @@ import type { AppProps } from 'next/app'
 
 import Layout from '../components/Layout';
 
+import MUITheme from '../MUITheme';
+
 import { ParallaxProvider } from 'react-scroll-parallax';
 import { MDXProvider } from '@mdx-js/react'
-import { createTheme, StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
+import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
 import AlertProvider from '../context/AlertContext';
 
 import { appWithTranslation } from 'next-i18next';
@@ -18,53 +20,6 @@ const MDXComponents = {
     h1: (props: any) => <h1 {...props} className="text-red-400" />,
     p: (props: HTMLAttributes<HTMLParagraphElement>) => <p {...props} className="my-2" />
 }
-
-const MUITheme = createTheme({
-    palette: {
-        primary: {
-            main: '#a8e1ff',
-        },
-        secondary: {
-            main: 'rgba(234,114,23,0.85)',
-        },
-        background: {
-            default: '#000000',
-            paper: '#161616',
-        },
-        text: {
-            primary: 'rgba(255,255,255,0.87)',
-        },
-    },
-    components: {
-        MuiLinearProgress: {
-            styleOverrides: {
-                root: {
-                    background: 'none'
-                }
-            }
-        },
-        MuiTextField: {
-            styleOverrides: {
-                root: {
-                    borderColor: '#rgba(255,255,255,.75)',
-                    '& .MuiOutlinedInput-root': {
-                        '& fieldset': {
-                            borderColor: 'rgba(255,255,255,.75)',
-                            borderRadius: 0,
-                        },
-                    },
-                    '& .MuiInputLabel-outlined': {
-                        color: 'rgba(255,255,255,.75)'
-                    },
-                    '& .Mui-disabled.MuiOutlinedInput-root': {
-                        background: '#ffffff57',
-                        color: 'rgba(255,255,255,.75)'
-                    }
-                },
-            },
-        }
-    }
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
