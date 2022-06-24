6a2fff73c906625ff80bb6b436723751cc0c0ed6
Matthew Wang <wmatthew123@gmail.com>
2022-06-24 14:18:41 +0800

    finished footer base

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import Head from 'next/head';
 
 import Header from './Header';
+import Footer from './Footer';
 
 import { useTranslation } from "next-i18next";
 import { useRouter } from 'next/router';
@@ -34,6 +35,7 @@ const Layout = ({ children }: LayoutProps) => {
             </Head>
             <Header />
             {children}
+            <Footer />
         </>
     )
 }
