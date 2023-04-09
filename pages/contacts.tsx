472b51fa85694198a8dd5c860f44163977894df9
Matthew Wang <matthew.wang@pickupp.io>
2023-04-09 21:07:57 +0800

    revert back to use page api route for contact

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -170,7 +170,7 @@ const ContactForm = ({
       if (!(await validateForm(input))) throw new Error("Invalid Input");
       console.log("sending form");
       setState(FormState.SENDING);
-      const res = await fetch("/newcontact", {
+      const res = await fetch("/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
