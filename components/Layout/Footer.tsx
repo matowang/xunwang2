d6cd7bb6c73a8b2f99f8edb591312988f74bf27e
Matthew Wang <matthew.wang@pickupp.io>
2023-04-09 12:27:49 +0800

    use material icon for youtube instead of svg

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -6,71 +6,84 @@ import { useRouter } from "next/router";
 import { useMemo } from "react";
 
 import SignatureImg from "../../public/images/xunwang-signature.png";
-import instagramIcon from "../../public/images/icons/icons8-instagram.svg";
+import { Instagram } from "@mui/icons-material";
 
 const Footer = () => {
-	const router = useRouter();
-	const { t } = useTranslation("common");
+  const router = useRouter();
+  const { t } = useTranslation("common");
 
-	const links: { name: string; href: string }[] = useMemo(
-		() =>
-			[
-				{
-					name: t("home"),
-					href: "/",
-				},
-				{
-					name: t("exhibits"),
-					href: "/exhibits",
-				},
-				{
-					name: t("contacts"),
-					href: "/contacts",
-				},
-				{
-					name: t("works"),
-					href: "/#works",
-				},
-			].filter((link) => link.href !== router.asPath),
-		[t, router]
-	);
+  const links: { name: string; href: string }[] = useMemo(
+    () =>
+      [
+        {
+          name: t("home"),
+          href: "/",
+        },
+        {
+          name: t("exhibits"),
+          href: "/exhibits",
+        },
+        {
+          name: t("contacts"),
+          href: "/contacts",
+        },
+        {
+          name: t("works"),
+          href: "/#works",
+        },
+      ].filter((link) => link.href !== router.asPath),
+    [t, router]
+  );
 
-	return (
-		<footer className='bg-black pt-32 relative text-xs sm:text-sm'>
-			<div className='flex flex-col items-center gap-8'>
-				<nav>
-					<ul className='flex'>
-						{links.map((link, i) => (
-							<li
-								key={`link${link.href}`}
-								className={`px-5 sm:px-10 ${
-									i === links.length - 1 ? "border-none" : "border-r-2 border-slate-400"
-								}`}>
-								<Link href={link.href}>{link.name}</Link>
-							</li>
-						))}
-					</ul>
-				</nav>
-				<a
-					className='text-white'
-					href='https://youtu.be/rXdJrMFju5Y'
-					target='_blank'
-					rel='noopener noreferrer'>
-					{t("watch youtube")}
-				</a>
-				<a href='https://www.instagram.com/xunwang2000/' target='_blank' rel='noopener noreferrer'>
-					<div className='flex gap-2'>
-						<Image className='opacity-70' src={instagramIcon} alt='instagram' />
-						{t("xun wang instagram")}
-					</div>
-				</a>
-			</div>
-			<div className='flex justify-between items-end py-20 px-10'>
-				<Image src={SignatureImg} alt="xun wang's signature" width={150} height={80} />
-				<p>© 2022 Xun Wang 王尋</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="relative bg-black pt-32 text-xs sm:text-sm">
+      <div className="flex flex-col items-center gap-8">
+        <nav>
+          <ul className="flex">
+            {links.map((link, i) => (
+              <li
+                key={`link${link.href}`}
+                className={`px-5 sm:px-10 ${
+                  i === links.length - 1
+                    ? "border-none"
+                    : "border-r-2 border-slate-400"
+                }`}
+              >
+                <Link href={link.href}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <a
+          className="text-white"
+          href="https://youtu.be/rXdJrMFju5Y"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t("watch youtube")}
+        </a>
+        <a
+          href="https://www.instagram.com/xunwang2000/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="flex gap-2">
+            <Instagram />
+            {t("xun wang instagram")}
+          </div>
+        </a>
+      </div>
+      <div className="flex items-end justify-between py-20 px-10">
+        <Image
+          src={SignatureImg}
+          alt="xun wang's signature"
+          width={150}
+          height={80}
+        />
+        <p>© 2022 Xun Wang 王尋</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,115 +11,130 @@ import { useRouter } from "next/router";
 
 import XunWorkingImg from "../public/images/xun-working.jpg";
 
-//import works from '../data/works.json';
+import { YouTube } from "@mui/icons-material";
+
 import works from "../data/works";
 
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const Home: NextPage = () => {
-	const { t } = useTranslation("home");
-	const { t: tCommon } = useTranslation("common");
+  const { t } = useTranslation("home");
+  const { t: tCommon } = useTranslation("common");
 
-	const { locale } = useRouter();
+  const { locale } = useRouter();
 
-	return (
-		<>
-			<video autoPlay muted loop className='fixed w-full h-screen object-cover -z-10'>
-				<source src='/hero-video.mp4' />
-				Your browser does not support the video tag.
-			</video>
-			<div className='w-full h-screen bg-gradient-to-t from-black via-transparent absolute top-0 left-0' />
-			<Parallax speed={50}>
-				<section className='relative w-full h-screen grid sm:grid-cols-2 justify-items-start align-end sm:flex-row sm:justify-between items-end p-10 sm:p-20'>
-					<h1 className='text-stone-300 text-3xl'>{tCommon("artistName")}</h1>
-					<a
-						className='sm:justify-self-end'
-						href='https://youtu.be/rXdJrMFju5Y'
-						target='_blank'
-						rel='noopener noreferrer'>
-						<SlideButton>
-							<div className='flex items-center gap-2'>
-								<img
-									src='/images/icons/icons8-youtube.svg'
-									alt='instagram'
-									className='h-6 w-6 opacity-70'
-								/>
-								{t("watchFullDoc")}
-							</div>
-						</SlideButton>
-					</a>
-				</section>
-			</Parallax>
-			<div className='bg-black text-white'>
-				<section className='items-center grid grid-cols-1 md:grid-cols-2 max-w-7xl mx-auto gap-10 px-5 py-20 md:py-60'>
-					<Parallax speed={10}>
-						<div className=''>
-							<Image
-								src={XunWorkingImg}
-								alt='xun wang working'
-								width={2000}
-								height={1333}
-								placeholder='blur'
-								style={{
-									maxWidth: "100%",
-									height: "auto",
-								}}
-							/>
-						</div>
-					</Parallax>
-					<Parallax speed={-5}>
-						<div className='flex flex-col gap-4 text-sm col-start-2'>
-							<p className='opacity-70'>{t("artistDescription1")}</p>
-							<p className='opacity-70'>{t("artistDescription2")}</p>
-							<Link href='/exhibits'>
-								<SlideButton className='mt-5'>{t("See Exhibit")}</SlideButton>
-							</Link>
-						</div>
-					</Parallax>
-				</section>
-				<div id='works' className='w-md p-2 max-w-7xl mx-auto'>
-					<Masonry columns={{ xs: 1, sm: 3, md: 4 }} spacing={{ xs: 0, sm: 1, md: 2 }}>
-						{works.map((work) => (
-							<div className='group relative text-white py-2 sm:py-0' key={work.id}>
-								<div>
-									<Image
-										alt={work.name}
-										src={work.staticImage}
-										placeholder='blur'
-										sizes='600px'
-										style={{
-											maxWidth: "100%",
-											height: "auto",
-										}}
-									/>
-								</div>
-								<div className='opacity-0 group-hover:opacity-100 grid transition-opacity grid-rows-2 grid-cols-2 absolute bg-black/30 w-full h-full p-8 top-0'>
-									<div className=''>
-										<h2>{work[(locale + "Name") as "enName" | "cnName"] ?? work.name}</h2>
-									</div>
-									<div className='justify-self-end'>{work.year}</div>
-									<Link
-										href={`/work/${work.id}`}
-										className='justify-self-end self-end col-span-full'>
-										<SlideButton className='px-2 py-1.5'>{tCommon("seeMore")}</SlideButton>
-									</Link>
-								</div>
-							</div>
-						))}
-					</Masonry>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <video
+        autoPlay
+        muted
+        loop
+        className="fixed -z-10 h-screen w-full object-cover"
+      >
+        <source src="/hero-video.mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="absolute top-0 left-0 h-screen w-full bg-gradient-to-t from-black via-transparent" />
+      <Parallax speed={50}>
+        <section className="align-end relative grid h-screen w-full items-end justify-items-start p-10 sm:grid-cols-2 sm:flex-row sm:justify-between sm:p-20">
+          <h1 className="text-3xl text-stone-300">{tCommon("artistName")}</h1>
+          <a
+            className="sm:justify-self-end"
+            href="https://youtu.be/rXdJrMFju5Y"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SlideButton>
+              <div className="flex items-center gap-2">
+                <YouTube />
+                {t("watchFullDoc")}
+              </div>
+            </SlideButton>
+          </a>
+        </section>
+      </Parallax>
+      <div className="bg-black text-white">
+        <section className="mx-auto grid max-w-7xl grid-cols-1 items-center gap-10 px-5 py-20 md:grid-cols-2 md:py-60">
+          <Parallax speed={10}>
+            <div className="">
+              <Image
+                src={XunWorkingImg}
+                alt="xun wang working"
+                width={2000}
+                height={1333}
+                placeholder="blur"
+                style={{
+                  maxWidth: "100%",
+                  height: "auto",
+                }}
+              />
+            </div>
+          </Parallax>
+          <Parallax speed={-5}>
+            <div className="col-start-2 flex flex-col gap-4 text-sm">
+              <p className="opacity-70">{t("artistDescription1")}</p>
+              <p className="opacity-70">{t("artistDescription2")}</p>
+              <Link href="/exhibits">
+                <SlideButton className="mt-5">{t("See Exhibit")}</SlideButton>
+              </Link>
+            </div>
+          </Parallax>
+        </section>
+        <div id="works" className="w-md mx-auto max-w-7xl p-2">
+          <Masonry
+            columns={{ xs: 1, sm: 3, md: 4 }}
+            spacing={{ xs: 0, sm: 1, md: 2 }}
+          >
+            {works.map((work) => (
+              <div
+                className="group relative py-2 text-white sm:py-0"
+                key={work.id}
+              >
+                <div>
+                  <Image
+                    alt={work.name}
+                    src={work.staticImage}
+                    placeholder="blur"
+                    sizes="600px"
+                    style={{
+                      maxWidth: "100%",
+                      height: "auto",
+                    }}
+                  />
+                </div>
+                <div className="absolute top-0 grid h-full w-full grid-cols-2 grid-rows-2 bg-black/30 p-8 opacity-0 transition-opacity group-hover:opacity-100">
+                  <div className="">
+                    <h2>
+                      {work[(locale + "Name") as "enName" | "cnName"] ??
+                        work.name}
+                    </h2>
+                  </div>
+                  <div className="justify-self-end">{work.year}</div>
+                  <Link
+                    href={`/work/${work.id}`}
+                    className="col-span-full self-end justify-self-end"
+                  >
+                    <SlideButton className="px-2 py-1.5">
+                      {tCommon("seeMore")}
+                    </SlideButton>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </Masonry>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-	return {
-		props: {
-			...(await serverSideTranslations(locale as string, ["home", "common"])),
-			// Will be passed to the page component as props
-		},
-	};
+  return {
+    props: {
+      ...(await serverSideTranslations(locale as string, ["home", "common"])),
+      // Will be passed to the page component as props
+    },
+  };
 };
 
 export default Home;
